ae2695ca3a6c571b9500825cd9adf873e3e22259
HMNikolova <HMNikolova@gmail.com>
2014-05-02 21:12:35 +0300

    small fix

---
+++ Introduction to Programming/6.PrintMyFullName/PrintMyFullName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PrintMyFullName
+{
+    static void Main()
+    {
+        Console.WriteLine("My first name is Hristina.");
+        Console.WriteLine("My last name is Nikolova.");
+    }
+}
+
