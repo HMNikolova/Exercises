748dc8d951854295f225ec1aeae54dac0dac8ccc
HMNikolova <HMNikolova@gmail.com>
2014-05-04 17:25:03 +0300

    new folder

---
+++ CSharpPartOne/1.Introduction to Programming/6.PrintMyFullName/PrintMyFullName.cs
@@ -0,0 +1,13 @@
+﻿// 6. Create console application that prints your first and last name.
+
+using System;
+
+class PrintMyFullName
+{
+    static void Main()
+    {
+        Console.WriteLine("My first name is Hristina.");
+        Console.WriteLine("My last name is Nikolova.");
+    }
+}
+
